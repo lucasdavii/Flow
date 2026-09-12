7e12e64605ad00d3ec8e3b2691579fc3657ee906
Belles-hub <belleximenes78@gmail.com>
2026-09-12 12:25:04 -0300

    feat: add FlowRoom frontend pages

---
+++ frontend/js/flowroom.js
@@ -0,0 +1,1 @@
+const go=(url)=>{window.location.href=url};document.querySelectorAll('[data-go]').forEach((el)=>el.addEventListener('click',()=>go(el.dataset.go)));document.querySelectorAll('form[data-next]').forEach((form)=>form.addEventListener('submit',(event)=>{event.preventDefault();go(form.dataset.next)}));
